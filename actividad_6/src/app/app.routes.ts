27c69f29fe1044e5c317f1020d13fdf0a022e44a
fkaexe <josemiguelmartinezay@gmail.com>
2025-09-09 12:15:03 +0200

    Rutas e Interface

--- actividad_6/src/app/app.routes.ts
+++ actividad_6/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { NewuserComponent } from './pages/newuser/newuser.component';
+import { UpdateuserComponent } from './pages/updateuser/updateuser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'user/:id', component: UserComponent},
+    {path: 'newuser', component: NewuserComponent},
+    {path: 'updateuser/:id',component: UpdateuserComponent},
+];
